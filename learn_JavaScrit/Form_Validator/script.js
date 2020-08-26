944e27f16a84b026330e2be59736a28c62b3c730
jaykiran <jaikiranjay@gmail.com>
2020-08-27 02:00:37 +0530

    javascript completed

--- learn_JavaScrit/Form_Validator/script.js
+++ learn_JavaScrit/Form_Validator/script.js
@@ -13,14 +13,90 @@ function showError(input, message) {
      small.innerText = message;
 }
 
+//show succcess outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//Email validity
+function checkEmail(input){
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value)){
+        showSuccess(input);
+    } else{
+        showError(input, 'Email is not Valid');
+    }
+}
+
+//get field Name
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+//check required fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+//check input length
+function checkLength(input, min, max) {
+    if(input.value.length < min){
+        showError(input, `${getFieldName(input)} must be atleast ${min} characters`);
+    } else if(input.value.length > max) {
+         showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+//check password
+function checkPasswordsMatch(input1, input2){
+    if(input1.value !== input2.value){
+        showError(input2, 'Passwords do not match');
+    }
+}
 
 //Event listerner
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
-    if(username.value === ''){
-        showError(username, 'enter username');
-    } else {
-        showSuccess(username); 
-    }
-})
+    checkRequired([username, email, password, password2]);
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
+
+    // if(username.value === ''){
+    //     showError(username, 'Username is required');
+    // } else {
+    //     showSuccess(username); 
+    // }
+
+    // if(email.value === ''){
+    //     showError(email, 'Email is required');
+    // } else if(!isValidEmail(email.value)) {
+    //     showError(email, 'Enter Valid Email Id');
+
+    // } else {
+    //     showSuccess(email); 
+    // }
+
+    // if(password.value === ''){
+    //     showError(password, 'Password is required');
+    // } else {
+    //     showSuccess(password); 
+    // }
+
+    // if(password2.value === ''){
+    //     showError(password2, 'Re-enter Password');
+    // } else {
+    //     showSuccess(password2); 
+    // }
+});
